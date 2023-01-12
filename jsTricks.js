98618ad8a84344ffc2ff929731a2698947386e7f
muhammadAmeen786 <mohdameeendar@gmail.com>
2023-01-12 09:44:40 +0530

    falsy bouncer

---
+++ jsTricks.js
@@ -0,0 +1,17 @@
+/* lets learn how to remove all the falsy values from an array.
+
+let arr = arr.filter(Boolean);
+*/
+
+
+let arrOfNums = [1,3,24,12,3,NaN,0,false];
+let booleans = arrOfNums.filter(bool);
+function bool(i){
+    if(!(i==0 || i==""|| i==NaN || i==undefined || i==null || i==false)){
+        return i;
+    }
+}
+
+console.log(booleans);
+
+console.log(arrOfNums.filter(Boolean)) // this is known as falsy bouncer.
