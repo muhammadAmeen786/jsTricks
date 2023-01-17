5f0902ff3af974908d3b09e1cf6ec9c74ae34175
muhammadAmeen786 <mohdameeendar@gmail.com>
2023-01-17 19:22:34 +0530

    inOperator in javascript object

---
+++ inOperatorInJs.js
@@ -0,0 +1,11 @@
+// If we want to check whether an object is present in Js object or not.
+// we can use In object.
+//like>
+let object1 = {
+    naam : "ameen",
+    kaam : "Student",
+    residence : "Kashmir"
+}
+
+console.log('kaam' in object1); // prints true becasue kaam naam ki property hai waha p.
+console.log('kaamnaam' in object1) ;// prints false on console
