83bf3fa46b28a36465aff4d21e28d0c1ca3bc561
muhammadAmeen786 <mohdameeendar@gmail.com>
2023-01-17 19:39:32 +0530

    Operator conversions file added

---
+++ operatorConversions.js
@@ -0,0 +1,22 @@
+//== type check nahee karta
+//=== type b check karta hai.
+
+
+
+console.log(1=='1') // true
+console.log(1==='1') // false
+console.log('' == 0); // true
+console.log(''==undefined) // false
+console.log(1==true)
+console.log("amaeen"==true)// false
+console.log([]==0); // true
+console.log([1]==true) // true
+
+// so to get the jist of it... 
+// there are various falsy values in Js. "",0,false, etc ar esome of these.
+console.log(typeof(undefined));// undefined
+console.log(typeof('')); //string
+console.log(typeof(NaN)); //number
+console.log(typeof(0)); //number
+console.log(typeof(1)); // number
+console.log(typeof(false)); // boolean
